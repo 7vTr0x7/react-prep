6e3283114b9167844e3848c39da020d748ba83ba
7vTr0x7 <7vtrox7@gmail.com>
2026-07-28 20:29:46 +0530

    Table

--- prep/src/components/Table.jsx
+++ prep/src/components/Table.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import "./style.css";
 
 const Table = () => {
   const [users, setUsers] = useState([]);
@@ -23,9 +24,32 @@ const Table = () => {
     getUsers();
   }, []);
 
+  const headers = users.length > 0 ? Object.keys(users[0]) : [];
+
   return (
     <div>
-      <table></table>
+      <table>
+        <thead>
+          <tr>
+            {headers?.length > 0 &&
+              headers.map((head, index) => <th key={index}>{head}</th>)}
+          </tr>
+        </thead>
+        <tbody>
+          {users?.length > 0 &&
+            users.map((user) => (
+              <tr key={user.id}>
+                {headers.map((head) => (
+                  <td key={head}>
+                    {typeof user[head] === "object"
+                      ? JSON.stringify(user[head])
+                      : user[head]}
+                  </td>
+                ))}
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </div>
   );
 };
