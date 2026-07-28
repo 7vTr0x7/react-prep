f874ab39025def98de2b8543268ac362b418a22c
7vTr0x7 <7vtrox7@gmail.com>
2026-07-28 20:10:03 +0530

    Table

--- prep/src/components/Table.jsx
+++ prep/src/components/Table.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Table = () => {
-  return <div></div>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        let res = await fetch("https://dummyjson.com/users");
+
+        if (!res.ok) {
+          throw new Error("user Fetch failed");
+        }
+
+        const data = await res.json();
+        if (data && data.users) {
+          setUsers(data.users);
+        }
+      } catch (error) {
+        console.log("Error occured while fetching users:", error);
+      }
+    };
+    getUsers();
+  }, []);
+
+  return (
+    <div>
+      <table></table>
+    </div>
+  );
 };
 
 export default Table;
