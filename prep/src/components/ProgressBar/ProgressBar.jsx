7b3342eed8318df9c175b538d070154b8a1f0b06
7vTr0x7 <7vtrox7@gmail.com>
2026-07-29 10:09:35 +0530

    ProgressBar

--- prep/src/components/ProgressBar/ProgressBar.jsx
+++ prep/src/components/ProgressBar/ProgressBar.jsx
@@ -1,10 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-const Progress = ({ value }) => {
+const Progress = ({ value, start, pause, reset, isRunning }) => {
   const percent = Math.min(100, Math.max(0, value));
 
   return (
-    <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        gap: "10px",
+      }}>
       <div
         style={{
           border: "1px solid black",
@@ -22,26 +30,65 @@ const Progress = ({ value }) => {
           <span>{percent}%</span>
         </div>
       </div>
+      <div style={{ display: "flex", gap: "10px" }}>
+        <button disabled={isRunning} onClick={() => start()}>
+          Start
+        </button>
+        <button disabled={!isRunning} onClick={() => pause()}>
+          Pause
+        </button>
+        <button disabled={value === 0} onClick={() => reset()}>
+          Reset
+        </button>
+      </div>
     </div>
   );
 };
 
 const ProgressBar = () => {
   const [value, setValue] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  let timer = useRef(null);
+
+  const start = () => {
+    if (!isRunning) {
+      if (value >= 100) return;
+
+      setIsRunning(true);
+      timer.current = setInterval(() => {
+        setValue((prev) => prev + 1);
+      }, 100);
+    }
+  };
+
+  const pause = () => {
+    if (isRunning) {
+      setIsRunning(false);
+      clearInterval(timer.current);
+    }
+  };
+
+  const reset = () => {
+    if (value > 0) {
+      clearInterval(timer.current);
+      setValue(0);
+      setIsRunning(false);
+    }
+  };
 
   useEffect(() => {
-    if (value >= 100) return;
-
-    let timer = setTimeout(() => {
-      setValue((prev) => prev + 1);
-    }, 100);
-
-    return () => clearTimeout(timer);
-  }, [value]);
-
+    return () => clearInterval(timer);
+  }, []);
   return (
     <div>
-      <Progress value={value} />
+      <Progress
+        value={value}
+        start={start}
+        pause={pause}
+        reset={reset}
+        isRunning={isRunning}
+      />
     </div>
   );
 };
