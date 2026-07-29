23eb87ded991cba6906e886c55b48d838727c7d1
7vTr0x7 <7vtrox7@gmail.com>
2026-07-29 10:33:45 +0530

    StopWatch

--- prep/src/components/StopWatch/StopWatch.jsx
+++ prep/src/components/StopWatch/StopWatch.jsx
@@ -1,5 +1,87 @@
+import { useEffect, useRef, useState } from "react";
+
+const formatTime = (time) => {
+  const hours = String(Math.floor(time / 3600)).padStart(2, "0");
+  const minutes = String(Math.floor((time % 3600) / 60)).padStart(2, "0");
+  const seconds = String(Math.floor(time % 60)).padStart(2, "0");
+
+  return `${hours}:${minutes}:${seconds}`;
+};
+
+const Watch = ({ timer, start, isRunning, pause, reset }) => {
+  const time = formatTime(timer);
+
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: "20px",
+        }}>
+        <h2>{time}</h2>
+        <div>
+          <button disabled={isRunning} onClick={() => start()}>
+            Start
+          </button>
+          <button disabled={!isRunning} onClick={() => pause()}>
+            Pause
+          </button>
+          <button disabled={time === 0} onClick={() => reset()}>
+            Reset
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const StopWatch = () => {
-  return <div></div>;
+  const [timer, setTimer] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  let interval = useRef(null);
+
+  const start = () => {
+    if (!isRunning) {
+      setIsRunning(true);
+      interval.current = setInterval(() => {
+        setTimer((prev) => prev + 1);
+      }, 100);
+    }
+  };
+
+  const pause = () => {
+    if (isRunning) {
+      setIsRunning(false);
+      clearInterval(interval.current);
+    }
+  };
+
+  const reset = () => {
+    if (timer > 0) {
+      setIsRunning(false);
+      clearInterval(interval.current);
+      setTimer(0);
+    }
+  };
+
+  useEffect(() => {
+    return () => clearInterval(interval.current);
+  }, []);
+
+  return (
+    <div>
+      <Watch
+        timer={timer}
+        start={start}
+        isRunning={isRunning}
+        pause={pause}
+        reset={reset}
+      />
+    </div>
+  );
 };
 
 export default StopWatch;
