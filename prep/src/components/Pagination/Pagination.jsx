2422ba33f9ae6ee2bfc7cdc5c75972ccab221138
7vTr0x7 <7vtrox7@gmail.com>
2026-08-07 16:51:08 +0530

    pagination

--- prep/src/components/Pagination/Pagination.jsx
+++ prep/src/components/Pagination/Pagination.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+const ProductCard = ({ prod }) => {
+  return <div style={{ border: "1px solid black", padding: "10px" }}></div>;
+};
 
 const Pagination = () => {
-  return <div></div>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("https://dummyjson.com/products");
+
+        if (!res.ok) {
+          console.log("failed to fetch");
+          return;
+        }
+        const data = await res.json();
+        if (data?.products) {
+          setProducts(data.products);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {products.length > 0 ? (
+        <div style={{ display: "flex", flexWrap: "wrap", gap: "5px" }}>
+          {products.map((prod) => (
+            <ProductCard key={prod.id} prod={prod} />
+          ))}
+        </div>
+      ) : (
+        <p>Not found</p>
+      )}
+    </div>
+  );
 };
 
 export default Pagination;
