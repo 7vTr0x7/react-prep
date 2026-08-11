2ae363bfab939bde66ecc5584e484873120f110f
7vTr0x7 <7vtrox7@gmail.com>
2026-08-11 12:13:42 +0530

    AutoComplete

--- prep/src/components/AutoComplete/AutoComplete.jsx
+++ prep/src/components/AutoComplete/AutoComplete.jsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const AutoComplete = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let res = await fetch("https://dummyjson.com/products");
+        if (!res.ok) {
+          throw new Error("Failed to fetch");
+        }
+        const result = await res.json();
+        if (result.products) {
+          setData(result.products);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return <div></div>;
 };
 
