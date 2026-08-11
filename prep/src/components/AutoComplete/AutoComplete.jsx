8577ddba7a305cc4c6676595c8cf3c715de7be00
7vTr0x7 <7vtrox7@gmail.com>
2026-08-11 14:46:10 +0530

    AutoComplete

--- prep/src/components/AutoComplete/AutoComplete.jsx
+++ prep/src/components/AutoComplete/AutoComplete.jsx
@@ -1,7 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
+
+const debounce = (fn, d) => {
+  let timer;
+
+  return function (...args) {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn(...args);
+    }, d);
+  };
+};
 
 const AutoComplete = () => {
   const [data, setData] = useState([]);
+  const [text, setText] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,9 +34,51 @@ const AutoComplete = () => {
     fetchData();
   }, []);
 
+  const textChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const debounced = debounce(
+    (text) =>
+      data.filter((d) => d.title.toLowerCase().includes(text.toLowerCase())),
+    300,
+  );
+
+  const filteredText = useMemo(() => debounced(text), [text]);
+
+  console.log(filteredText);
   return (
-    <div style={{ width: "50%", display: "flex", justifyContent: "center" }}>
-      <div style={{ border: "1px solid black" }}></div>
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        padding: "100px",
+        boxSizing: "border-box",
+      }}>
+      <div
+        style={{
+          border: "1px solid black",
+          padding: "10px",
+          width: "50%",
+          height: "auto",
+        }}>
+        <div>
+          <input
+            type="text"
+            placeholder="Search . . ."
+            value={text}
+            onChange={(e) => textChange(e)}
+          />
+        </div>
+        {text && filteredText && (
+          <div style={{ overflowY: "auto" }}>
+            {filteredText.map((t) => (
+              <p key={t.id}>{t.title}</p>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
