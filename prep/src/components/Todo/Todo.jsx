4203f09163caa656ccb3cea3cfb1263038471329
7vTr0x7 <7vtrox7@gmail.com>
2026-08-12 17:06:31 +0530

    Todo

--- prep/src/components/Todo/Todo.jsx
+++ prep/src/components/Todo/Todo.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { use, useState } from "react";
 
 const Todo = () => {
-  return <div></div>;
+  const [todo, setTodo] = useState([]);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        padding: "auto",
+      }}>
+      <div
+        style={{
+          border: "1px solid black",
+          padding: "20px",
+          width: "50%",
+          height: "auto",
+          boxSizing: "border-box",
+          borderRadius: "10px",
+        }}>
+        <div>
+          <input
+            type="text"
+            placeholder="Add todo"
+            style={{
+              width: "100%",
+              boxSizing: "border-box",
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Todo;
