afc7187e75b035093fd9629da12578002ba35d97
7vTr0x7 <7vtrox7@gmail.com>
2026-08-13 17:53:54 +0530

    useFetchData

--- prep/src/components/CustomHook/CustomHook.jsx
+++ prep/src/components/CustomHook/CustomHook.jsx
@@ -1,5 +1,17 @@
+import { useMemo } from "react";
+import useFetchData from "./useFetchData";
+
 const CustomHook = () => {
-  return <div></div>;
+  const [data] = useMemo(() => useFetchData(), []);
+  return (
+    <div>
+      {data.map((d) => (
+        <div key={d.id}>
+          <p>{d.title}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default CustomHook;
--- prep/src/components/CustomHook/useFetchData.jsx
+++ prep/src/components/CustomHook/useFetchData.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const useFetchData = () => {
-  return <div></div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("https://dummyjson.com/products");
+        if (!res.ok) {
+          throw new Error("Failed fetch");
+        }
+
+        const result = await res.json();
+        if (result?.products) {
+          setData(result.products);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return [data];
 };
 
 export default useFetchData;
