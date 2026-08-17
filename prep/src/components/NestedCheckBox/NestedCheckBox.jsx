018233b2dc1037e88f6d5cf34654a387aba04b9a
7vTr0x7 <7vtrox7@gmail.com>
2026-08-17 17:52:28 +0530

    NestedCheckBox

--- prep/src/components/NestedCheckBox/NestedCheckBox.jsx
+++ prep/src/components/NestedCheckBox/NestedCheckBox.jsx
@@ -1,18 +1,32 @@
 import React, { useState } from "react";
 import { checkboxData } from "./data";
 
-const CheckBoxes = React.memo(({ checkboxes }) => {
-  console.log("n");
+const CheckBoxes = React.memo(({ checkboxes, checked, setChecked }) => {
+  console.log("n", checked);
   return (
     <div>
       {checkboxes.map((c) => (
         <div key={c.id}>
           <label htmlFor={c.id}>
-            <input type="checkbox" name={c.id} id={c.id} />
+            <input
+              type="checkbox"
+              name={c.id}
+              id={c.id}
+              checked={checked[c.id] || false}
+              onChange={() =>
+                setChecked((prev) => ({ ...prev, [c.id]: !prev[c.id] }))
+              }
+            />
             {c.name}
           </label>
           <div style={{ padding: "0px 20px" }}>
-            {c.children && <CheckBoxes checkboxes={c.children} />}
+            {c.children && (
+              <CheckBoxes
+                checkboxes={c.children}
+                checked={checked}
+                setChecked={setChecked}
+              />
+            )}
           </div>
         </div>
       ))}
@@ -22,9 +36,14 @@ const CheckBoxes = React.memo(({ checkboxes }) => {
 
 const NestedCheckBox = () => {
   const [checkboxes, setCheckboxes] = useState(checkboxData);
+  const [checked, setChecked] = useState({});
   return (
     <div>
-      <CheckBoxes checkboxes={checkboxes} />
+      <CheckBoxes
+        checkboxes={checkboxes}
+        checked={checked}
+        setChecked={setChecked}
+      />
     </div>
   );
 };
