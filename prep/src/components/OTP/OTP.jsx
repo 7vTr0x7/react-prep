33b2e6d7f4d3edc3fc2149fe66bc836b1c7b7df9
7vTr0x7 <7vtrox7@gmail.com>
2026-08-17 17:18:46 +0530

    OTP

--- prep/src/components/OTP/OTP.jsx
+++ prep/src/components/OTP/OTP.jsx
@@ -1,16 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const OPT_DIGIT_COUNT = 5;
 
 const OTP = () => {
   const [digits, setDigits] = useState(new Array(OPT_DIGIT_COUNT).fill(""));
+  const dRef = useRef([]);
 
   const handleOnChange = (v, i) => {
     if (isNaN(v)) return;
+    let value = v.trim();
     const newArr = [...digits];
-    newArr[i] = v.slice(-1);
+    newArr[i] = value.slice(-1);
     setDigits(newArr);
+    value && dRef.current[i + 1]?.focus();
   };
+
+  const back = (e, i) => {
+    if (e.key === "Backspace") {
+      dRef.current[i - 1]?.focus();
+    }
+  };
+
+  useEffect(() => {
+    dRef.current[0]?.focus();
+  }, []);
 
   return (
     <div>
@@ -22,8 +35,11 @@ const OTP = () => {
         }}>
         {digits.map((d, i) => (
           <input
+            ref={(input) => {
+              dRef.current[i] = input;
+            }}
             type="text"
-            value={digits[i]}
+            value={d}
             key={i}
             style={{
               height: "30px",
@@ -33,6 +49,7 @@ const OTP = () => {
               fontWeight: "bold",
             }}
             onChange={(e) => handleOnChange(e.target.value, i)}
+            onKeyDown={(e) => back(e, i)}
           />
         ))}
       </div>
