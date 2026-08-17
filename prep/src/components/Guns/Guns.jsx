ab968530c3d2f3e44bb590dcd4c5e300b9f1b07d
7vTr0x7 <7vtrox7@gmail.com>
2026-08-17 14:17:38 +0530

    Guns

--- prep/src/components/Guns/Guns.jsx
+++ prep/src/components/Guns/Guns.jsx
@@ -1,7 +1,56 @@
-import React from "react";
+import { useState } from "react";
+
+const NormalGun = ({ normalGun, setNormalGun }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: "10px",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}>
+      <button onClick={() => setNormalGun((prev) => prev + 1)}>Normal</button>
+      <span>{normalGun}</span>
+    </div>
+  );
+};
 
 const Guns = () => {
-  return <div></div>;
+  const [normalGun, setNormalGun] = useState(0);
+  const [debouncedGun, setDebouncedGun] = useState(0);
+  const [throttledGun, setThrottledGun] = useState(0);
+
+  return (
+    <div
+      style={{
+        width: "20%",
+        display: "flex",
+        flexDirection: "column",
+        gap: "20px",
+      }}>
+      <NormalGun normalGun={normalGun} setNormalGun={setNormalGun} />
+      <div
+        style={{
+          display: "flex",
+          gap: "10px",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}>
+        <button>Debounced</button>
+        <span>{debouncedGun}</span>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          gap: "10px",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}>
+        <button>Throttled</button>
+        <span>{throttledGun}</span>
+      </div>
+    </div>
+  );
 };
 
 export default Guns;
