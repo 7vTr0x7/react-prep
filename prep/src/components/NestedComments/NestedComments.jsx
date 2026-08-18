5364f2833afcea3b60c0ef1b2d629d99465148a3
7vTr0x7 <7vtrox7@gmail.com>
2026-08-18 16:06:53 +0530

    NestedComments

--- prep/src/components/NestedComments/NestedComments.jsx
+++ prep/src/components/NestedComments/NestedComments.jsx
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import { data } from "./data";
+
+const Comments = ({ comments }) => {
+  return (
+    <div>
+      {comments.map((comment) => (
+        <div key={comment.id} style={{ width: "200px" }}>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                gap: "10px",
+              }}>
+              <div
+                style={{
+                  fontSize: "20px",
+                  fontWeight: "bold",
+                  width: "30px",
+                  height: "30px",
+                  textAlign: "center",
+                  backgroundColor: "lightgray",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  borderRadius: "50%",
+                }}>
+                <span>{comment.author.name.charAt(0)}</span>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}>
+                <span>{comment.author.name}</span>
+                <span>{comment.comment}</span>
+              </div>
+            </div>
+            <span>{comment.date}</span>
+          </div>
+          <div style={{ padding: "10px 20px" }}>
+            {comment.replies && <Comments comments={comment.replies} />}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 const NestedComments = () => {
-  return <div></div>;
+  const [comments, setComments] = useState(data);
+
+  return (
+    <div style={{ padding: "100px" }}>
+      <Comments comments={comments} />
+    </div>
+  );
 };
 
 export default NestedComments;
---
+++ prep/src/components/NestedComments/data.js
@@ -0,0 +1,55 @@
+export const data = [
+  {
+    id: 1,
+    author: {
+      name: "Non",
+      email: "non@gmail.com",
+    },
+    date: "1h ago",
+    comment: "non1",
+    replies: [
+      {
+        id: 11,
+        author: {
+          name: "Non",
+          email: "non@gmail.com",
+        },
+        date: "1h ago",
+        comment: "non1",
+        replies: [
+          {
+            id: 111,
+            author: {
+              name: "Non",
+              email: "non@gmail.com",
+            },
+            date: "1h ago",
+            comment: "non1",
+            replies: [
+              {
+                id: 1111,
+                author: {
+                  name: "Non",
+                  email: "non@gmail.com",
+                },
+                date: "1h ago",
+                comment: "non1",
+                replies: [
+                  {
+                    id: 11111,
+                    author: {
+                      name: "Non",
+                      email: "non@gmail.com",
+                    },
+                    date: "1h ago",
+                    comment: "non1",
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
