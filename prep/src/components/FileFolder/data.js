2455304e42c02d912d7cb2f5a00305142e931f47
7vTr0x7 <7vtrox7@gmail.com>
2026-08-18 11:04:54 +0530

    FileFolder

---
+++ prep/src/components/FileFolder/data.js
@@ -0,0 +1,21 @@
+export const data = [
+  {
+    id: 1,
+    name: "Root",
+    isFolder: true,
+    children: [
+      {
+        id: 11,
+        name: "src",
+        isFolder: true,
+        children: [
+          {
+            id: 111,
+            name: "App.jsx",
+            isFolder: false,
+          },
+        ],
+      },
+    ],
+  },
+];
