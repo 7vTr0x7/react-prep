7e49cf1017194d973de2306937a153abeabf802c
7vTr0x7 <7vtrox7@gmail.com>
2026-08-18 11:28:56 +0530

    FileFolder

--- prep/src/components/FileFolder/FileFolder.jsx
+++ prep/src/components/FileFolder/FileFolder.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { dataFiles } from "./data";
+
+const Files = ({ files }) => {
+  return (
+    <div>
+      {files.map((node) => (
+        <div key={node.id}>
+          <div style={{ display: "flex", alignItems: "center", gap: "20px" }}>
+            <span>{"+"}</span>
+            <span>{node.name}</span>
+            <span>{"📂"}</span>
+            <span>{"📄"}</span>
+          </div>
+          <div style={{ padding: "0px 20px" }}>
+            {node.children && <Files files={node.children} />}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 const FileFolder = () => {
-  return <div></div>;
+  const [files, setFiles] = useState(dataFiles);
+
+  return (
+    <div>
+      <Files files={files} />
+    </div>
+  );
 };
 
 export default FileFolder;
